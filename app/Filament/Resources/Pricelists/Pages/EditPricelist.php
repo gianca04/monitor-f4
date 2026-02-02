d23098ef1472a7ea2334923a98071176a72af80c
gianca04 <grancarlosavalogallardo@gmail.com>
2026-02-02 15:12:24 -0500

    cambios comentados de delete en edit de resource

--- app/Filament/Resources/Pricelists/Pages/EditPricelist.php
+++ app/Filament/Resources/Pricelists/Pages/EditPricelist.php
@@ -13,7 +13,7 @@ class EditPricelist extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            DeleteAction::make(),
+            //  DeleteAction::make(),
         ];
     }
 }
